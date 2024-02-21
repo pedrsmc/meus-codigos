3cb1483ba2b65d6294f79ac7592ce8de0d4ae2da
Pedro Samuel <128701224+pedrsmc@users.noreply.github.com>
2024-02-21 15:04:53 -0300

    Delete web/js testes directory

--- web/js testes/nco/main.js
+++
@@ -1,9 +0,0 @@
-const idade = null
-
-// usando o operador "ou":
-
-// document.body.innerHTML = "Idade: " + (idade || "Náo informado.")
-
-// nullish coalescing operator:
-
-document.body.innerHTML = "Idade: " + (idade ?? "Não informado")
