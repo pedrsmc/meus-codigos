31f31934f76e2977c556e0764d406b3756e0c1e7
Pedro Samuel <128701224+pedrsmc@users.noreply.github.com>
2024-03-12 23:25:56 -0300

    Delete java/java codes directory

--- java/java codes/src/Soma.java
+++
@@ -1,9 +0,0 @@
-public class Soma {
-
-    public static void main(String[] args) {
-       int x = 10;
-       int y = 10;
-       
-       System.out.printf("Soma dos dois valores: %d\n", (x + y));
-    }
-}
