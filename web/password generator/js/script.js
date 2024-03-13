e02ea5ebe6c47889d464a2261f73637cd4533abc
Pedro Samuel <128701224+pedrsmc@users.noreply.github.com>
2024-03-12 23:10:31 -0300

    Add files via upload

--- web/password generator/js/script.js
+++ web/password generator/js/script.js
@@ -1,9 +1,7 @@
 const generateBtn = document.querySelector('#generateBtn')
 const password = document.querySelector('#password')
-const inputBox = document.querySelector('.input-box')
-const copyBtn = document.querySelector('#copyBtn')
 const lengthInput = document.querySelector('#length')
-const advice = document.querySelector("#advice")
+const lengthText = document.querySelector("#lengthText")
 
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 const lowerCase = "abcdefghijklmnopqrstuvwxyz"
@@ -12,40 +10,10 @@ const symbol = "!@#$%^&*()_+{}][=-<>/"
 
 const allChars = upperCase + lowerCase + number + symbol
 
-generateBtn.addEventListener('click', validation)
-password.addEventListener('click', verifyFirtClick)
-copyBtn.addEventListener('click', verifyFirtClick)
 
-function validation() {
-    let value = lengthInput.value
-
-    if (value == "") {
-        inputBox.style.border = "2px solid #ca1010"
-        advice.textContent = "Digite a quantidade de caracteres que deseja que a senha possua."
-    } else {
-        if (value < 6) {
-            inputBox.style.border = "2px solid #ca1010"
-            advice.textContent = "Quantidade de caracteres muito baixa!"
-        } else if (value > 50) {
-            inputBox.style.border = "2px solid #ca1010"
-            advice.textContent = "Quantidade de caracteres muito alta!"
-        } else {
-            inputBox.style.border = ""
-            advice.textContent = ""
-            generatePassword(value)
-        }
-    }
-}
-
-function verifyFirtClick() {
-    if (password.value != "") {
-        copyPassword()
-    } else {
-    }
-}
-
-function generatePassword(length) {
-    length = lengthInput.value
+generateBtn.addEventListener('click', () => {
+    password.style.border = "none"
+    let length = lengthInput.value
     let newPassword = ""
 
     newPassword += upperCase[Math.floor(Math.random() * upperCase.length)]
@@ -58,10 +26,23 @@ function generatePassword(length) {
     }
 
     password.value = newPassword
-}
+    password.style.cursor = "pointer"
+})
+
+password.addEventListener('click', () => {
+    if (password.value != "") {
+        copyPassword()
+    }
+})
+
+lengthInput.addEventListener('input', () => {
+    const length = lengthInput.value;
+    lengthText.innerHTML = `${length} caracteres`;
+});
 
 function copyPassword() {
     password.select()
     document.execCommand("copy")
-    alert('A senha gerada foi copiada com sucesso.')
+    
+    password.style.border = "solid 2px #008cff"
 }
