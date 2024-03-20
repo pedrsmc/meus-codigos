bc4445782e51303fbb59de117f2dbbc2fc965b84
Pedro Samuel <128701224+pedrsmc@users.noreply.github.com>
2024-03-19 22:49:08 -0300

    Delete java/codigos java directory

--- java/codigos java/IdiomaAtual.java
+++
@@ -1,9 +0,0 @@
-import java.util.Locale;
-
-public class IdiomaAtual {
-    public static void main(String[] args) {
-        Locale language = Locale.getDefault();
-
-        System.out.println("O idioma atual do computador é: " + language.getDisplayName());
-    }
-}
