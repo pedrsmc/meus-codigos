4c30f52b3b36c8bcb40d852bf728f8956c7fcff1
Pedro Samuel <128701224+pedrsmc@users.noreply.github.com>
2024-04-01 09:53:23 -0300

    Add files via upload

--- web/landing page/js/main.js
+++ web/landing page/js/main.js
@@ -1,8 +1,40 @@
 window.addEventListener('scroll', function () {
-    var navbar = document.querySelector('.navbar');
+    let navbar = document.querySelector('.navbar')
+
     if (window.scrollY > 0) {
-        navbar.classList.add('shadow');
+        navbar.classList.add('shadow')
     } else {
-        navbar.classList.remove('shadow');
+        navbar.classList.remove('shadow')
     }
+})
+document.addEventListener('DOMContentLoaded', function () {
+    let paragraphs = document.querySelectorAll('.text-field p, .text-field span')
+
+    paragraphs.forEach(function (paragraphs) {
+        paragraphs.style.opacity = "100%"
+    })
+})
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    let serviceFields = document.querySelectorAll('.service-field, #our-plans')
+    
+    const fadeInElementsWhenVisible = () => {
+        serviceFields.forEach(function (element, index) {
+            let bounding = element.getBoundingClientRect()
+
+            if (bounding.top >= 0 && bounding.bottom <= window.innerHeight) {
+                setTimeout(function () {
+                    element.style.opacity = "100%"
+                }, index * 300)
+            }
+        });
+    }
+
+    fadeInElementsWhenVisible()
+
+    window.addEventListener('scroll', function () {
+        fadeInElementsWhenVisible()
+    });
 });
+
